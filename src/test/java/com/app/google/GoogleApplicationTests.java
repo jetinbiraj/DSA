7b8c2488e1e8d40644531c6f6b13ce4a8ee056b4
jkbiraj <birajdarjk1106@gmail.com>
2024-05-22 22:24:37 +0530

    Initial commit

---
+++ src/test/java/com/app/google/GoogleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.google;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoogleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
